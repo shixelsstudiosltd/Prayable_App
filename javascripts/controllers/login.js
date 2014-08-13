b57b4e550b81c6e56ef2a89894b08e636abf9b6a
khanani92 <muddassir_92@hotmail.com>
2014-08-14 00:35:15 +0500

    form is taking and validating the data through  input

--- javascripts/controllers/login.js
+++ javascripts/controllers/login.js
@@ -1,7 +1,23 @@
 sampleApp.controller('login',function($rootScope,$scope,$location,$http){
     $scope.userData = {email:'',password:''};
-    
+    $scope.errorMsg = ''
+    $scope.loginUser = function(){
+        if(($scope.userData.email) && (validateEmail($scope.userData.email))){
+            if(($scope.userData.password) && ($scope.userData.password.length > 5 && $scope.userData.password.length < 15)){
+                $scope.errorMsg = "";
+                console.log('login Function Called '+' email: '+$scope.userData.email+' Password: '+$scope.userData.password)
+            }else{
+                $scope.errorMsg = "*Mistake in password"
+            }
+        }else{
+            $scope.errorMsg = "*Mistake in email"
+        }
 
+    }
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
     $scope.go = function (path){
         $location.path(path);
     }
