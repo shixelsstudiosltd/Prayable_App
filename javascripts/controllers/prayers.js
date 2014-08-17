7f68d92102d74f87442965bcb5bc0e87ce3671a5
Fara Ashiru <fara@shixels.com>
2014-08-17 11:18:45 -0400

    fixed some mobile view issues.

--- javascripts/controllers/prayers.js
+++ javascripts/controllers/prayers.js
@@ -1,6 +1,8 @@
 sampleApp.controller('prayers',function($rootScope,$scope,$location){
 
-
+		$scope.back = function (path){
+	        history.back();
+		};
 
         $scope.go = function (path){
             $location.path(path);
