59fc1aa164c5b4368dff85641d18741b0e383ee3
khanani92 <muddassir_92@hotmail.com>
2014-09-11 23:18:56 +0500

    working on pray page

--- javascripts/controllers/group_popup.js
+++ javascripts/controllers/group_popup.js
@@ -2,7 +2,7 @@ sampleApp.controller('group_popup',function($rootScope,$scope,$location,ngDialog
 
 
 
-        $scope.go = function (path){
+    $scope.go = function (path){
             $location.path(path);
         }
 
--- javascripts/controllers/pray.js
+++ javascripts/controllers/pray.js
@@ -1,4 +1,4 @@
-sampleApp.controller('pray',function($rootScope,$scope,$location,ngDialog){
+sampleApp.controller('pray',function($rootScope,$scope,$location,ngDialog,$http){
     $scope.prayerInfo = {
         user_id:'',
         prayer_text:'',
@@ -14,12 +14,49 @@ $scope.savePrayer =function(){
     console.log($scope.prayerInfo)
 }
     $scope.selectGrp =function(){
-        ngDialog.open({
-            template: './partials/groups_popup.html',
-            className: 'group_popup',
-            scope: $scope
-        });
+        var data = {userid:'540c61ca5b0845fb2a16bdd1'}
+        $http({
+            method:"POST",
+            //contentType: 'application/json',
+            url:"http://localhost:3000/allGroup",
+            // url:"http://prayable-21641.onmodulus.net/allGroup",
+            data:data,
+            crossDomain: true,
+            dataType: "json"
+        }).success(function(data, textstatus) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $scope.groupList = data;
+                console.log(data)
+                ngDialog.open({
+                    template: './partials/groups_popup.html',
+                    className: 'group_popup',
+                    scope: $scope
+                });
+
+                // console.log(textstatus)
+            }).error(function(data, textstatus) {
+
+                //console.log(data)
+                console.log(textstatus)
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
     }
+    $scope.selection = []
+    $scope.toggleSelection = function toggleSelection(selectionID) {
+        var idx = $scope.selection.indexOf(selectionID);
+
+        // is currently selected
+        if (idx > -1) {
+            $scope.selection.splice(idx, 1);
+        }
+
+        // is newly selected
+        else {
+            $scope.selection.push(selectionID);
+        }
+    };
     $scope.selectjrl =function(){
         ngDialog.open({
             template: './partials/journal_popup.html',
@@ -41,6 +78,8 @@ $scope.selectfeed =function(){
             scope: $scope
         });
     }
+
+
 
 
 
