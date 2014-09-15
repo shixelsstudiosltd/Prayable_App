f303986b9ae016d57f1470b0d823dbec31bd707e
khanani92 <muddassir_92@hotmail.com>
2014-09-16 00:42:34 +0500

    journal popup is connected with API and selection working

--- javascripts/controllers/pray.js
+++ javascripts/controllers/pray.js
@@ -43,33 +43,78 @@ $scope.savePrayer =function(){
                 // or server returns response with an error status.
             });
     }
-    $scope.selection = []
-    $scope.toggleSelection = function toggleSelection(selectionID) {
-        var idx = $scope.selection.indexOf(selectionID);
+    $scope.gselection = [];
+    $scope.jselection= []
+    $scope.toggleSelection = function toggleSelection(selectionID,key) {
+        var tempData;
+        if(key == 'g'){
+            tempData = $scope.gselection
+        }else if(key == 'j'){
+            tempData=$scope.jselection
+        }
+        var idx = tempData.indexOf(selectionID);
 
         // is currently selected
         if (idx > -1) {
-            $scope.selection.splice(idx, 1);
+            tempData.splice(idx, 1);
+            trasferData(tempData,key)
         }
 
         // is newly selected
         else {
-            $scope.selection.push(selectionID);
+            tempData.push(selectionID);
+            trasferData(tempData,key)
         }
 
     };
 
-    $scope.saveGroup = function(){
-        $scope.prayerInfo.group_id = $scope.selection
+    function trasferData(tempData,key){
+        if(key =='g'){
+            $scope.gselection = tempData
+        }else if(key == 'j'){
+            $scope.jselection= tempData
+        }
+    }
+
+    $scope.saveSelection = function(key){
+        if(key == 'g'){
+            $scope.prayerInfo.group_id = $scope.gselection
+        }else if(key == 'j'){
+            $scope.prayerInfo.journal_id = $scope.jselection
+        }
+        //console.log($scope.prayerInfo)
     }
 
 
     $scope.selectjrl =function(){
-        ngDialog.open({
-            template: './partials/journal_popup.html',
-            className: 'journal_popup',
-            scope: $scope
-        });
+        var data = {userid:'540c61ca5b0845fb2a16bdd1'}
+        $http({
+            method:"POST",
+            //contentType: 'application/json',
+            url:"http://localhost:3000/allJournal",
+            // url:"http://prayable-21641.onmodulus.net/allJournal",
+            data:data,
+            crossDomain: true,
+            dataType: "json"
+        }).success(function(data, textstatus) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $scope.journalList = data;
+                //console.log(data)
+                ngDialog.open({
+                    template: './partials/journal_popup.html',
+                    className: 'journal_popup',
+                    scope: $scope
+                });
+
+                // console.log(textstatus)
+            }).error(function(data, textstatus) {
+
+                //console.log(data)
+                console.log(textstatus)
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
     }
     $scope.selectmsg =function(){
         ngDialog.open({
