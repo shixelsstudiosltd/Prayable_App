101e3201796c8c560d9e15ad9dc62d0cbab53c40
khanani92 <muddassir_92@hotmail.com>
2014-10-31 23:16:43 +0500

    API for room is made and connected to /inbox still working on it

--- javascripts/controllers/inbox.js
+++ javascripts/controllers/inbox.js
@@ -1,4 +1,40 @@
-sampleApp.controller('inbox',function($rootScope,$scope,$location){
+sampleApp.controller('inbox',function($rootScope,$scope,$location,$http){
+
+
+    var userData =  JSON.parse(sessionStorage.getItem('userData'));
+    $scope.isLogged = false
+    if(userData && (Object.keys(userData).length > 0)){
+        $scope.noRooms = false;
+        $scope.isLogged = true;
+        $scope.room = '';
+        $scope.msgStatus = ''
+        var data = {userID:userData._id}
+        $scope.openRoom = function(id){
+            alert("a"+id)
+        }
+
+        $http({
+            method:"POST",
+            //contentType: 'application/json',
+            //url:"http://localhost:3000/getRoom",
+            url:"http://prayable-21641.onmodulus.net/getRoom",
+            data:data,
+            crossDomain: true,
+            dataType: "json"
+        }).success(function(room, textstatus) {
+                $scope.rooms = room
+                for(var i =0; i < room.length; i++  ){
+                   if($scope.rooms[i].latestMessage.is_read == false){$scope.rooms[i].status = 'new'}else{$scope.rooms[i].status = ''}
+                }
+                    console.log(room)
+        })
+
+
+    }else{
+        $location.path('/login')
+        if(!$scope.$$phase) $scope.$apply();
+    }
+
 
 
 
