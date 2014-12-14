d52561609843d1b01d897b4ae1efcfe3f2639f34
khanani92 <muddassir_92@hotmail.com>
2014-12-14 22:11:38 +0500

    like and unlike function have been implemented. If the user  click the star the server check if that prayer is already liked or not and like and unlike acc to it and make changes on the client side

--- javascripts/controllers/prayerCat.js
+++ javascripts/controllers/prayerCat.js
@@ -16,11 +16,67 @@ sampleApp.controller('prayerCat',function($rootScope,$scope,$location,$http){
         }
 
 
+        $scope.createStar = function(payerID){
+          var data ={prayerID:payerID, userID:userData._id}
+            $http({
+                method:"POST",
+                //contentType: 'application/json',
+                //url:"http://localhost:3000/saveLike",
+                url:"http://prayable-21641.onmodulus.net/saveLike",
+                data:data,
+                crossDomain: true,
+                dataType: "json"
+            }).success(function(data, textstatus) {
+
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    //console.log(data)
+                if(data.status == 400){
+                    //console.log('some error in server please retry')
+                }else if(data.status == 201){
+                    //console.log('This prayer is already liked by you')
+
+                    var prayerIDTemp = data.data.prayerID
+                    var prayerListTemp = $scope.prayerList
+                    var prayerTemp = prayerListTemp.filter(function(prayer) {
+                        if(prayerIDTemp === prayer.prayerInfo._id){
+                            prayer.likeCount --;
+                        }  // filter out appropriate one
+
+                    });
+                    if((prayerTemp)&&(prayerTemp.length > 0) ){
+                        $scope.prayerList = prayerListTemp
+                    }
+
+                    }else{//data.status == 200
+                    var prayerIDTemp = data.data.prayerID
+                    var prayerListTemp = $scope.prayerList
+                    var prayerTemp = prayerListTemp.filter(function(prayer) {
+                        if(prayerIDTemp === prayer.prayerInfo._id){
+                            prayer.likeCount ++;
+                        }  // filter out appropriate one
+
+                    });
+                    if((prayerTemp)&&(prayerTemp.length > 0) ){
+                        $scope.prayerList = prayerListTemp
+                    }
+                    //$scope.prayerList.likeCount ++;
+                    }
+                    // $scope.prayerSerachList = data;
+                }).error(function(data, textstatus) {
+
+                    //console.log(data)
+                    // console.log(textstatus)
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
+        }
+
         $http({
             method:"POST",
             //contentType: 'application/json',
-            url:"http://localhost:3000/getPrayer",
-            //url:"http://prayable-21641.onmodulus.net/getPrayer",
+            //url:"http://localhost:3000/getPrayer",
+            url:"http://prayable-21641.onmodulus.net/getPrayer",
             data:data,
             crossDomain: true,
             dataType: "json"
