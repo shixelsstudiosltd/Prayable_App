1f2bddc36a6640cba196a978dabccf126cc52bdb
khanani92 <muddassir_92@hotmail.com>
2014-12-15 13:22:58 +0500

    like and comment count and likie/unlike function is also added to individual prayer page.

--- javascripts/controllers/prayer.js
+++ javascripts/controllers/prayer.js
@@ -33,6 +33,52 @@ sampleApp.controller('prayer',function($rootScope,$scope,$location,$http){
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
             });
+
+        $scope.createStar = function(payerID){
+            var data ={prayerID:payerID, userID:userData._id}
+            $http({
+                method:"POST",
+                //contentType: 'application/json',
+                url:"http://localhost:3000/saveLike",
+                //url:"http://prayable-21641.onmodulus.net/saveLike",
+                data:data,
+                crossDomain: true,
+                dataType: "json"
+            }).success(function(data, textstatus) {
+
+                    // this callback will be called asynchronously
+                    // when the response is available
+                    //console.log(data)
+                    if(data.status == 400){
+                        //console.log('some error in server please retry')
+                    }else if(data.status == 201){
+                        //console.log('This prayer is already liked by you')
+
+                        var prayerIDTemp = data.data.prayerID
+                            if(prayerIDTemp === $scope.prayerData.prayerInfo._id){
+                                $scope.prayerData.likeCount --;
+                            }  // filter out appropriate one
+
+                    }else{//data.status == 200
+                        var prayerIDTemp = data.data.prayerID
+
+                            if(prayerIDTemp === $scope.prayerData.prayerInfo._id){
+                                $scope.prayerData.likeCount ++;
+                            }  // filter out appropriate one
+
+                        //$scope.prayerList.likeCount ++;
+                    }
+                    // $scope.prayerSerachList = data;
+                }).error(function(data, textstatus) {
+
+                    //console.log(data)
+                    // console.log(textstatus)
+                    // called asynchronously if an error occurs
+                    // or server returns response with an error status.
+                });
+        }
+
+
     }else{
         $location.path('/')
         if(!$scope.$$phase) $scope.$apply();
