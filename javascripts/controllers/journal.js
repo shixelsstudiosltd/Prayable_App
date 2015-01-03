8e195326a4daad3f8b64971b5cea48056cd56828
khanani92 <muddassir_92@hotmail.com>
2015-01-03 17:32:58 +0500

    Changes made in journal API acc to the html and connected to HTML. still working on it

--- javascripts/controllers/journal.js
+++ javascripts/controllers/journal.js
@@ -1,8 +1,46 @@
-sampleApp.controller('journal',function($rootScope,$scope,$location){
+sampleApp.controller('journal',function($rootScope,$scope,$location,$http){
     $scope.back = function (path){
         history.back();
         $scope.$apply();
     };
+
+    $scope.isLogged = false
+    $scope.showSearch = false;
+    var userData =  JSON.parse(sessionStorage.getItem('userData'));
+    if(userData && (Object.keys(userData).length > 0)){
+
+        $scope.journalList = '';
+        $http({
+            method:"POST",
+            //contentType: 'application/json',
+            //url:"http://localhost:3000/allJournal",
+            url:"http://prayable-21641.onmodulus.net/allJournal",
+            data:{userID:userData._id},
+            crossDomain: true,
+            dataType: "json"
+        }).success(function(data, textstatus) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $scope.isLogged = true;
+                $scope.showSearch = true;
+                $scope.journalList = data;
+
+                 console.log(data)
+            }).error(function(data, textstatus) {
+
+                //console.log(data)
+                console.log(textstatus)
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+
+
+    }else{
+        $location.path('/')
+        if(!$scope.$$phase) $scope.$apply();
+    }
+
+
 
 
 
