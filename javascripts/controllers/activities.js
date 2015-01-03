30c022ecf9050ff929183fec674b33f6c2e2704c
khanani92 <muddassir_92@hotmail.com>
2015-01-03 14:12:47 +0500

    BAckend for Activity have been created with createActivity, getAllActivities, getActivityDetail, getActivityByType and deleteActivity
    BAckend for Activity Type have been created with
    createActivityType, deleteActivityType
    
    BAckend for Activity Tag have been created with
    createActivityTag, deleteActivityTag
    
    Activities.html is connected with the getAllActivities to all activities of users.

--- javascripts/controllers/activities.js
+++ javascripts/controllers/activities.js
@@ -1,9 +1,34 @@
-sampleApp.controller('activities',function($rootScope,$scope,$location){
+sampleApp.controller('activities',function($rootScope,$scope,$location,$http){
+    var userData =  JSON.parse(sessionStorage.getItem('userData'));
+    $scope.isLogged = false
 
 
+    if(userData && (Object.keys(userData).length > 0)){
+        $scope.isLogged = true;
+        $scope.activities = '';
 
-        $scope.go = function (path){
-            $location.path(path);
-        }
+        $http({
+            method:"POST",
+            //contentType: 'application/json',
+            ///url:"http://localhost:3000/getAllActivities",
+            url:"http://prayable-21641.onmodulus.net/getAllActivities",
+            data:{userID:userData._id},
+            crossDomain: true,
+            dataType: "json"
+        }).success(function(activities, textstatus) {
+                console.log(activities)
+                $scope.activities = activities
 
+            }).error(function(err,data){
+
+
+            })
+
+    }else{
+        $location.path('/login')
+        if(!$scope.$$phase) $scope.$apply();
+    }
+    $scope.go = function (path){
+        $location.path(path);
+    }
 })
